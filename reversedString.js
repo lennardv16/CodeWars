1158d1046ade98892c3a17cd3470a62bd2227f55
Lennard Vicente <94157241+lennardv16@users.noreply.github.com>
2023-12-08 03:06:41 -0500

    Returns reversed string

---
+++ reversedString.js
@@ -0,0 +1,7 @@
+function reversedString(str) {
+  console.log(str.split('').reverse().join(''));
+}
+
+reversedString('racecar');
+reversedString('oogabooga');
+reversedString('the quick brown fox jumped over the lazy dog');
