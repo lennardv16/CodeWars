93baa6fe0d35eee8fa815fd822e0618617165ec6
Lennard Vicente <94157241+lennardv16@users.noreply.github.com>
2023-12-08 02:51:17 -0500

    refactored code

--- multiples.js
+++ multiples.js
@@ -1,19 +1,11 @@
 function solution(number) {
-  if (number < 0) {
-    return 0;
-  }
-
   let sum = 0;
-  for (let i = 1; i < number; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      sum += i;
-    } else if (i % 3 === 0) {
-      sum += i;
-    } else if (i % 5 === 0) {
+  for (let i = 3; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
   }
   return sum;
 }
 
-console.log(solution(20));
+console.log(solution(90));
